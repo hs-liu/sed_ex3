a5dffbcd1f5388020268b2655a9bf63cd41dba59
hl2622, Hongshuo Liu <hongshuo.liu22@imperial.ac.uk>
2023-10-25 17:08:14 +0100

    add test for triangle number

---
+++ src/test/java/ic/doc/strategy/TriangleNumberSequenceTest.java
@@ -0,0 +1,48 @@
+package ic.doc.strategy;
+
+import ic.doc.strategy.TriangleNumberSequence;
+import org.junit.Test;
+
+import static ic.doc.matchers.IterableBeginsWith.beginsWith;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
+
+public class TriangleNumberSequenceTest {
+  final TriangleNumberSequence sequence = new TriangleNumberSequence();
+
+  @Test
+  public void definesFirstTwoTermsToBeOne() {
+
+    assertThat(sequence.term(0), is(1));
+    assertThat(sequence.term(1), is(3));
+  }
+
+  @Test
+  public void definesSubsequentTermsToBeTheSumOfThePreviousTwo() {
+
+    assertThat(sequence.term(2), is(6));
+    assertThat(sequence.term(3), is(10));
+    assertThat(sequence.term(4), is(15));
+    System.out.println(sequence.iterator().next());
+    System.out.println(sequence.iterator().next());
+
+  }
+
+  @Test
+  public void isUndefinedForNegativeIndices() {
+
+    try {
+      sequence.term(-1);
+      fail("should have thrown exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e.getMessage(), containsString("Not defined for indices < 0"));
+    }
+  }
+
+  @Test
+  public void canBeIteratedThrough() {
+    assertThat(sequence, beginsWith(1, 3, 6, 10, 5));
+  }
+}
